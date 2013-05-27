4388a50fb4c0e354f1d0d4b85da0aebd057410bd
Liu Chunchen <lcclcc0520@163.com>
2013-05-27 22:11:32 +0800

    Configure and activate Service module

--- Apps/Conf/config.php
+++ Apps/Conf/config.php
@@ -9,7 +9,7 @@ return array(
     'DB_PORT'                   =>  '3306',
     'DB_PREFIX'                 =>  'think_',
     'APP_AUTOLOAD_PATH'         =>  '@.TagLib',
-    'APP_GROUP_LIST'            =>  'Home,Maintain',
+    'APP_GROUP_LIST'            =>  'Home,Maintain,Service',
     'DEFAULT_GROUP'             =>  'Maintain',
     'APP_GROUP_MODE'            =>  1,
     'SHOW_PAGE_TRACE'           =>  1//显示调试信息
