195ddcb6185405c019f78775f91d7f6b54f8f338
Liu Chunchen <lcclcc0520@163.com>
2013-05-27 22:09:47 +0800

    Add Service Module to provide data access api for js code

---
+++ Apps/Modules/Service/Action/IndexAction.class.php
@@ -0,0 +1,12 @@
+<?php
+class IndexAction extends Action{
+    public function index() {
+        $lostCard = M('LostCard');
+        //$data = $lostCard->select(array('id'=>1));
+        $data = $lostCard->select();
+        //$this->assign('title', 'Home');
+        //$this->assign('data', $data);
+        print json_encode($data);
+        //$this->display();
+    }
+}
