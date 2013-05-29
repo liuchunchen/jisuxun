8e6c3c341809ab925f11014c29658d7cb8a035d8
Liu Chunchen <lcclcc0520@163.com>
2013-05-29 22:31:20 +0800

    Modify Tpl and combined with DB connection

--- Apps/Modules/Index/Action/IndexAction.class.php
+++ Apps/Modules/Index/Action/IndexAction.class.php
@@ -1,7 +1,25 @@
 <?php
 class IndexAction extends Action{
+    private static $colors = array("block_red", "block_blue", "block_yellow", "block_green");
     public function index() {
         $this->assign('title', '首页-极速寻');
+        $lostCard = M('LostCard');
+        $cards = $lostCard->select();
+        $i = 0;
+        $max = rand(10, 30);
+        //print $max . "<br/>";
+        $data = array();
+        while($i < $max) {
+            $i = $i + 1;
+            $color = rand(0, 3);
+            //print_r(self::$colors);
+            //print self::$colors[$color] . "<br/>";
+            $tmp['color'] = self::$colors[$color];
+            $tmp['content'] = self::$colors[$color];
+            $tmp['card'] = $cards[$i % 2];
+            $data[] = $tmp;
+        }
+        $this->assign('data', $data);
         $this->display();
     }
 }
